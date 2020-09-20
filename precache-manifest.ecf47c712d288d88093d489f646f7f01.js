5472d9f4691c301a7dfa3d08111419e7020e5862
Toyin Abdulsalam <toyinabdulsalam@Toyins-MacBook-Pro.local>
2020-09-20 18:50:16 +1000

    Updates

---
+++ precache-manifest.ecf47c712d288d88093d489f646f7f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bb093fb317f985665cbb1dbc12bc8f",
+    "url": "/Amazon-Clone/index.html"
+  },
+  {
+    "revision": "e09daac413840182d437",
+    "url": "/Amazon-Clone/static/css/main.a10ed847.chunk.css"
+  },
+  {
+    "revision": "6c486a434c96f4f13185",
+    "url": "/Amazon-Clone/static/js/2.ea6f2308.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/Amazon-Clone/static/js/2.ea6f2308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09daac413840182d437",
+    "url": "/Amazon-Clone/static/js/main.19efe477.chunk.js"
+  },
+  {
+    "revision": "4cb599ab17126a081400",
+    "url": "/Amazon-Clone/static/js/runtime-main.300eae63.js"
+  }
+]);
